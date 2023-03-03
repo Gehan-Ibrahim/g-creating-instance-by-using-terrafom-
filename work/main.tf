0b86a3f8d07b1241e85ee79bc7aa05778664ebef
“Gehan-Ibrahim” <gehanibrahim276@gmail.com>
2023-03-03 15:06:50 +0200

    inital commit

---
+++ work/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "my-ec2"{
+    ami="ami-0f3c9c466bb525749"
+    instance_type="t2.micro"
+
+tags = {
+    Name = "gehan-ec2"
+ }
+
+}
+resource  "aws_eip" "my-eip"{
+    vpc = true
+}
+resource "aws_eip_association" "associate"{
+    instance_id=aws_instance.my-ec2.id
+    allocation_id=aws_eip.my-eip.id
+
+}
+
