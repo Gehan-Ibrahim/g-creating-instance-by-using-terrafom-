b4000d88eb2b40347bec3141cf11b4fa59eb1a9f
gehan ibrahim <102517464+Gehan-Ibrahim@users.noreply.github.com>
2023-03-03 16:12:19 +0200

    Update main.tf

--- work/main.tf
+++ work/main.tf
@@ -15,4 +15,24 @@ resource "aws_eip_association" "associate"{
     allocation_id=aws_eip.my-eip.id
 
 }
+output "subnet-id" {
+    description = "VMs Private IP"
+    value= aws_instance.my-ec2.subnet_id
+
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.my-ec2.private_ip
+}
+output "public_value" {
+    description = "VMs Public IP"
+    value= aws_instance.my-ec2.public_ip
+}
+terraform {
+    backend "s3" {
+    bucket = "gehanbucket"
+    key    = "gehan-key"
+    region = "us-east-2"
+  }
+}
 
